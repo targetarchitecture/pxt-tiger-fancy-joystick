ad390aaa3ba38ceac77ef4be0d24b6d25542d7ad
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-02-07 21:16:29 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,22 @@ radio.setFrequencyBand(76)
 radio.setGroup(76)
 radio.setTransmitSerialNumber(true)
 radio.sendValue("hello", 1)
-basic.forever(function () {
-	
+basic.showIcon(IconNames.Heart)
+
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("MICROBIT_ID_BUTTON_A")
 })
+
+
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("MICROBIT_ID_BUTTON_B")
+})
+
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("MICROBIT_ID_BUTTON_AB")
+})
+
+input.onGesture(Gesture.Shake, function() {
+    radio.sendString("MICROBIT_ID_BUTTON_AB")
+})
+
