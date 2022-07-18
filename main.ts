0076b604e51381401030ffdb456acc94c0153ea3
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-07-18 21:43:54 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
-let leftx = 0
-function DetectJoystickCommands () {
-    leftx = 0
-}
+let left_x = 0
+let left_y = 0
+let right_x = 0
+let right_y = 0
 
+function DetectJoystickCommands () {
+    left_x = joystick.ReadStickAxis(Stick.JOYSTICK_LEFT, Axis.JOYSTICK_X_AXIS)
+    left_y = joystick.ReadStickAxis(Stick.JOYSTICK_LEFT, Axis.JOYSTICK_Y_AXIS)
+    right_x = joystick.ReadStickAxis(Stick.JOYSTICK_RIGHT, Axis.JOYSTICK_X_AXIS)
+    right_y = joystick.ReadStickAxis(Stick.JOYSTICK_RIGHT, Axis.JOYSTICK_Y_AXIS)
+}
 
 function sendRadioMessage(message: string) {
     radio.sendString(message)
@@ -16,6 +22,8 @@ radio.setFrequencyBand(76)
 radio.setGroup(76)
 radio.setTransmitSerialNumber(true)
 sendRadioMessage("JOYSTICK_START")
+
+basic.showIcon(IconNames.Heart)
 
 basic.forever(function () {
     DetectJoystickCommands()
