cea2be0051f998065ba99400d0e035b87ee913b6
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-07-18 14:56:22 +0100

    Update pxt.json, joystickbit.ts

---
+++ joystickbit.ts
@@ -0,0 +1,14 @@
+//% color=#FF6EC7 weight=100 icon="\uf004" block="JoyStick"
+//% category="JoyStick"
+//% subcategories='["Play"]'
+namespace JoyStick {
+
+    /**
+      * Add into the start function to initialise the board.
+      */
+    //% block="Start Rainbow Sparkle Unicorn"
+    export function _sendMessage(message: string): void {
+       // _MSGTOSEND.push(message);
+    }
+
+}
